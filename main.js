cae7e074f3633bc65e7485cf06e308df7a5cf9ed
Azamat <muratovichazamat@yandex.com>
2021-07-01 12:34:56 +0500

    js-tutorial

---
+++ main.js
@@ -0,0 +1,16 @@
+const slides = document.querySelectorAll('.slide');
+
+
+for (const slide of slides) {
+  slide.addEventListener('click', () => {
+    clearActiveClasses()
+
+    slide.classList.add('active')
+  })
+}
+
+function clearActiveClasses() {
+  slides.forEach(function (slide) {
+    slide.classList.remove('active')
+  })
+}
