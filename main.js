ae75b635f970dd892a7ff02365d48c1a5be86fe3
Azamat <muratovichazamat@yandex.com>
2021-07-01 15:47:18 +0500

    added function  slidesPlugin

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlugin(activeSlide) {
 
+   const slides = document.querySelectorAll('.slide');
+   slides[activeSlide].classList.add('active')
 
-for (const slide of slides) {
-  slide.addEventListener('click', () => {
-    clearActiveClasses()
+   for (const slide of slides) {
+      slide.addEventListener('click', () => {
+         clearActiveClasses()
 
-    slide.classList.add('active')
-  })
+         slide.classList.add('active')
+      })
+   }
+
+   function clearActiveClasses() {
+      slides.forEach(function (slide) {
+         slide.classList.remove('active')
+      })
+   }
 }
-
-function clearActiveClasses() {
-  slides.forEach(function (slide) {
-    slide.classList.remove('active')
-  })
-}
+slidesPlugin(0)
